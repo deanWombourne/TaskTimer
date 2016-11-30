1d6bcd67071f4af00e2fbe1d16be0eb8d63e4833
Sam Dean <deanWombourne@gmail.com>
2016-11-30 21:57:47 +0000

    Initial project creation

---
+++ TaskTimer/TaskTimer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TaskTimer
+//
+//  Created by Sam Dean on 30/11/2016.
+//  Copyright © 2016 deanWombourne. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
