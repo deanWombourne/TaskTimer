0eeb8bcc5a20a1c9a3f130e550b3c0272e873d84
Sam Dean <deanWombourne@gmail.com>
2016-12-22 14:41:48 +0000

    Update fastlane

--- TaskTimer/TaskTimer/Scenes/HomeViewController.swift
+++ TaskTimer/TaskTimer/Scenes/HomeViewController.swift
@@ -9,6 +9,45 @@
 import Foundation
 import UIKit
 
+
+private enum Section {
+    case active(tasks: [Task])
+    case recent(tasks: [Task])
+
+    var count: Int {
+        switch self {
+        case .active(let tasks): return tasks.count
+        case .recent(let tasks): return tasks.count
+        }
+    }
+}
+
+
 final class HomeViewController: UIViewController {
+
+    fileprivate var sections: [Section] = []
+
+}
+
+
+extension HomeViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.sections.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.sections[section].count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        return cell
+    }
+}
+
+
+extension HomeViewController: UITableViewDelegate {
 
 }
