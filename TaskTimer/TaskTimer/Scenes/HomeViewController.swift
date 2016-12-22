82a18ffc2dd265995429651dcfb9457cfdb0f80f
Sam Dean <deanWombourne@gmail.com>
2016-12-22 12:20:35 +0000

    Added nav controllers

---
+++ TaskTimer/TaskTimer/Scenes/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FirstViewController.swift
+//  TaskTimer
+//
+//  Created by Sam Dean on 30/11/2016.
+//  Copyright © 2016 deanWombourne. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+}
---
+++ TaskTimer/TaskTimer/Scenes/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  TaskTimer
+//
+//  Created by Sam Dean on 30/11/2016.
+//  Copyright © 2016 deanWombourne. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+}
